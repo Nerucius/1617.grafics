5e0027febc76c32a255e92f7e34f9df14611fb0e
GERMAN DEMPERE GUILLERMO <gdempegu11.alumnes@ub052229.mat.ub.edu>
2017-05-29 19:19:18 +0200

    Changed Atmosphere and Earth Colors

--- Practica 3/globe/globe.js
+++ Practica 3/globe/globe.js
@@ -13,20 +13,25 @@
 
 var DAT = DAT || {};
 
+// TODO Global variables for Earth and Atmosphere, to change uniforms
+
 DAT.Globe = function(container, opts) {
   opts = opts || {};
   
   var colorFn = opts.colorFn || function(x) {
     var c = new THREE.Color();
-    c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
+    c.setHSL( 0, 1.0, ( 0.6 - ( x * 0.5 ) ) );
+    
+    // c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
     return c;
   };
+  
   var imgDir = opts.imgDir || '/globe/';
 
   var Shaders = {
     'earth' : {
       uniforms: {
-        'texture': { type: 't', value: null }
+        'texture': { type: 't', value: null },
       },
       vertexShader: [
         'varying vec3 vNormal;',
@@ -43,8 +48,9 @@ DAT.Globe = function(container, opts) {
         'varying vec2 vUv;',
         'void main() {',
           'vec3 diffuse = texture2D( texture, vUv ).xyz;',
+          'diffuse.r *= 1.2;',
           'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+          'vec3 atmosphere = vec3( 1.0, .0, .0 ) * pow( intensity, 3.0 );',
           'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
         '}'
       ].join('\n')
@@ -62,7 +68,7 @@ DAT.Globe = function(container, opts) {
         'varying vec3 vNormal;',
         'void main() {',
           'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+          'gl_FragColor = vec4( 1.0, .3, .3, 1.0 ) * intensity;',
         '}'
       ].join('\n')
     }
